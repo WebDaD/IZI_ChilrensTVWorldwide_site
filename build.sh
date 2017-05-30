707c7dbce5854506160484d1598eb984c6eefe32
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-05-30 16:18:31 +0200

    Added Flags, some fixes

--- build.sh
+++ build.sh
@@ -3,20 +3,25 @@ echo "Creating Public Folders"
 mkdir -p public
 mkdir -p public/images
 mkdir -p public/pdf
+mkdir -p public/flags
 mkdir -p public/fonts
 mkdir -p public/css
 mkdir -p public/js
 mkdir -p tmp
 chmod 777 tmp/
 
+echo "Refresh Bower packages"
+bower install
+
 echo "Creating Index.html"
 pug index.pug -O keys.json -O studies.json -o public/
 
 echo "Combine, Minify and Copy CSS"
 touch tmp/main.css
-lessc main.less tmp/main.css
+lessc assets/css/main.less tmp/main.css
 cp bower_components/bootstrap/dist/css/bootstrap.min.css tmp/bootstrap.min.css
 cp bower_components/font-awesome/css/font-awesome.min.css tmp/font-awesome.min.css
+cp bower_components/flag-icon-css/css/flag-icon.min.css tmp/flag-icon.min.css
 cp -a bower_components/font-awesome/fonts/. public/fonts/
 uglifycss tmp/*.css > public/css/main.css
 
@@ -29,10 +34,16 @@ cp -a assets/images/. public/images/
 echo "Copy PDFs"
 cp -a assets/pdfs/. public/pdfs/
 
+echo "Copy Flags"
+cp -a bower_components/flag-icon-css/flags/. public/flags/
+
 echo "Copy Impressum and Datenschutz"
 pug impressum.pug -o public/
 pug datenschutz.pug -o public/
 
+echo "Copy 404 and 500"
+pug 404.pug -o public/
+pug 500.pug -o public/
 
 echo "Copy htaccess"
 cp .htaccess public/.htaccess
