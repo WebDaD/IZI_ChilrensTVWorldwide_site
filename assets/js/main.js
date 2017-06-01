02ef5faa958b32f936dfacc8915e6d26ccb10bfe
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-06-01 09:06:52 +0200

    v3

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,7 @@ $(document).ready(function () {
       window.location.hash = haash
     })
   })
+  $('.nav a').on('click', function () {
+    $('.navbar-toggle').click() // bootstrap 3.x by Richard
+  })
 })
